9631a525a817f836e8516092e662bbc0f5392f4c
Pramod D P <80121@DESKTOP-931H0VQ.exalca.corp>
2020-10-05 15:34:22 +0530

    env changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     hmr       : false,
-    baseAddress : 'http://10.20.1.212:6003/v1/',
+    baseAddress : 'http://10.20.1.212:8001/v1/',
     clientId : 'ngAuthApp'
 };
